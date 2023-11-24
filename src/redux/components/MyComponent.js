e210846a9a1ce7cc7b98d53c19359aea26a5dc7e
Quan_KMA <itquankma@gmail.com>
2023-11-24 10:33:10 +0700

    #16 React componnent

---
+++ src/redux/components/MyComponent.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+
+class MyComponent extends React.Component{
+    render(){
+        return(
+            <h3>Hello world</h3>
+        )
+    };
+}
+
+export default MyComponent;
